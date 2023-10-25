2e6b3d285b5e46d3936b233126ef2467aaaa05ea
Pavel <p.filippov2.20@gmail.com>
2023-10-25 21:00:50 +0300

    feat: array start

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -19,8 +19,10 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  const index = arr.findIndex((val) => val === value);
+  // const index = arr.indexOf(value);
+  return index;
 }
 
 /**
@@ -34,8 +36,8 @@ function findElement(/* arr, value */) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len }, (_, index) => 2 * index + 1);
 }
 
 /**
@@ -50,8 +52,9 @@ function generateOdds(/* len */) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  const dArr = [...arr];
+  return [...arr, ...dArr];
 }
 
 /**
@@ -65,8 +68,14 @@ function doubleArray(/* arr */) {
  *    [-1, 2, -5, -4, 0] => [ 2 ]
  *    [] => []
  */
-function getArrayOfPositives(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfPositives(arr) {
+  const posValue = arr.reduce((result, num) => {
+    if (num > 0) {
+      result.push(num);
+    }
+    return result;
+  }, []);
+  return posValue;
 }
 
 /**
@@ -80,8 +89,14 @@ function getArrayOfPositives(/* arr */) {
  *    [ 1, 2, 3, 4, 5 ] => []
  *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
  */
-function getArrayOfStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfStrings(arr) {
+  const stringValue = arr.reduce((result, str) => {
+    if (typeof (str) === 'string') {
+      result.push(str);
+    }
+    return result;
+  }, []);
+  return stringValue;
 }
 
 /**
